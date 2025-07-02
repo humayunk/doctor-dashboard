e702587f1248651d5d376f6faef9e5c3a15c6a4a
Ryan P. O'Connell <ryan@healthdatasafe.org>
2025-07-02 16:02:08 -0400

    Remove New/Edit form screen and related buttons
    Related subtasks:
    - https://app.asana.com/1/1210391646975052/project/1210391646975104/task/1210699025040870
    - https://app.asana.com/1/1210391646975052/project/1210391646975104/task/1210699025040871
    - https://app.asana.com/1/1210391646975052/project/1210391646975104/task/1210699025040877

--- src/app/forms/form/page.jsx
+++ src/app/forms/form/page.jsx
@@ -7,11 +7,6 @@ export default function Page() {
       <Tabbar tabs={props.form.tabs} />
       <div className="m-4">
         <p className="my-2 text-4xl text-gray-900 dark:text-white">Intake Form</p>
-        <a href="/forms/form/edit">
-          <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Edit</button>
-        </a>
-        <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Publish</button>
-        <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Advanced Settings</button>
         <p className="my-8 text-4xl text-gray-900 dark:text-white">Profile</p>
         <form className="max-w-md">
           <div className="relative z-0 w-full mb-5 group">
