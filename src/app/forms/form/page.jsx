fc6804c76d1ac6dda87d107bdecbbb8b30f6789c
Ryan P. O'Connell <ryan@healthdatasafe.org>
2025-07-02 18:19:16 -0400

    Add Description and Consent sections
    https://app.asana.com/1/1210391646975052/project/1210391646975104/task/1210699025040873

--- src/app/forms/form/page.jsx
+++ src/app/forms/form/page.jsx
@@ -6,9 +6,13 @@ export default function Page() {
     <>
       <Tabbar tabs={props.form.tabs} />
       <div className="m-4">
-        <p className="my-2 text-4xl text-gray-900 dark:text-white">Intake Form</p>
-        <p className="my-8 text-4xl text-gray-900 dark:text-white">Profile</p>
+        <p className="my-4 text-4xl text-gray-900 dark:text-white">Intake Form</p>
+        <p className="my-2 text-2xl text-gray-900 dark:text-white">Description</p>
+        <p>This form comprises the initial intake questions before your first visit with Dr. Doe.</p>
+        <p className="my-2 text-2xl text-gray-900 dark:text-white">Consent</p>
+        <p>This form requires read & write access to Profile data.</p>
         <form className="max-w-md">
+          <p className="mt-8 text-3xl text-gray-900 dark:text-white">Profile</p>
           <div className="relative z-0 w-full mb-5 group">
             <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
             <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
