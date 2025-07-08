3014713bfdb250499f950e9ad53771c84f2b8cc5
Ryan P. O'Connell <ryan@healthdatasafe.org>
2025-07-08 12:57:28 -0400

    Extrapolate all mapped components into functions

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,15 @@
 import { IconForms, IconHamburger, IconLifesaver, IconLogout, IconPeople } from "@/components/ui/icons"
 
+function FormEntry({ href, name }) {
+  return (
+    <li key={name}>
+      <a href={href} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        {name}
+      </a>
+    </li>
+  )
+}
+
 function Sidebar({ forms }) {
   return (
     <>
@@ -26,12 +36,8 @@ function Sidebar({ forms }) {
                 <span className="flex-1 ms-3 whitespace-nowrap">Forms</span>
               </div>
             </li>
-            {forms.map(({ name, href }) => (
-              <li key={name}>
-                <a href={href} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                  {name}
-                </a>
-              </li>
+            {forms.map(({ href, name }) => (
+              <FormEntry key={name} href={href} name={name} />
             ))}
           </ul>
           <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -8,18 +8,24 @@ function Body({ data }) {
   return (
     <tbody>
       {data.map((row) => (
-        <tr key={row[first]} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-          <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            <Status status={row[first]} />
-          </th>
-          {keys.slice(1).map((key) => (
-            <td key={key} className="px-6 py-4">
-              <Status status={row[key]} />
-            </td>
-          ))}
-        </tr>
+        <TableBody key={row[first]} first={first} keys={keys} row={row} />
       ))}
     </tbody>
+  )
+}
+
+function DropdownMenu({ column, first, options }) {
+  return (
+    <div id={`dropdown${column}`} className="normal-case z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600">
+      <ul className="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200">
+        <li>
+          <a href="#">
+            <IconSort />
+          </a>
+        </li>
+      </ul>
+      <Filter options={column !== first ? options : null} />
+    </div>
   )
 }
 
@@ -30,16 +36,22 @@ function Filter({ options }) {
   return (
     <ul className="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownCheckboxButton">
       {options.map((option) => (
-        <li key={option}>
-          <div className="flex items-center">
-            <input defaultChecked id="checkbox-item-1" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-            <label htmlFor="checkbox-item-1" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              {option}
-            </label>
-          </div>
-        </li>
+        <FilterOption key={option} option={option} />
       ))}
     </ul>
+  )
+}
+
+function FilterOption({ option }) {
+  return (
+    <li>
+      <div className="flex items-center">
+        <input defaultChecked id="checkbox-item-1" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+        <label htmlFor="checkbox-item-1" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          {option}
+        </label>
+      </div>
+    </li>
   )
 }
 
@@ -48,28 +60,24 @@ function Header({ columns, options }) {
     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
       <tr>
         {columns.map((column) => (
-          <th key={column} scope="col" className="px-6 py-3">
-            <div className="flex items-center">
-              {column}
-              <button id={`dropdownDefaultButton${column}`} data-dropdown-toggle={`dropdown${column}`}>
-                <IconFilter />
-              </button>
-              {/* dropdown menu */}
-              <div id={`dropdown${column}`} className="normal-case z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600">
-                <ul className="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200">
-                  <li>
-                    <a href="#">
-                      <IconSort />
-                    </a>
-                  </li>
-                </ul>
-                <Filter options={column !== columns[0] ? options : null} />
-              </div>
-            </div>
-          </th>
+          <HeaderRow key={column} column={column} first={columns[0]} options={options} />
         ))}
       </tr>
     </thead>
+  )
+}
+
+function HeaderRow({ column, first, options }) {
+  return (
+    <th scope="col" className="px-6 py-3">
+      <div className="flex items-center">
+        {column}
+        <button id={`dropdownDefaultButton${column}`} data-dropdown-toggle={`dropdown${column}`}>
+          <IconFilter />
+        </button>
+        <DropdownMenu column={column} first={first} options={options} />
+      </div>
+    </th>
   )
 }
 
@@ -103,6 +111,27 @@ function Table({ props, props: { columns, data, options } }) {
         </table>
       </div>
     </div>
+  )
+}
+
+function TableBody({ first, keys, row }) {
+  return (
+    <tr key={row[first]} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        <Status status={row[first]} />
+      </th>
+      {keys.slice(1).map((key) => (
+        <TableData key={key} item={key} row={row} />
+      ))}
+    </tr>
+  )
+}
+
+function TableData({ item, row }) {
+  return (
+    <td className="px-6 py-4">
+      <Status status={row[item]} />
+    </td>
   )
 }
 
