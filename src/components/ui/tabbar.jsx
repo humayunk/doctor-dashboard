5b9256167b5042e76afaa3b1660fcc9523a1fbd7
Ryan P. O'Connell <ryan@healthdatasafe.org>
2025-07-11 03:42:02 -0400

    Indicate active tab based on path

--- src/components/ui/tabbar.jsx
+++ src/components/ui/tabbar.jsx
@@ -1,12 +1,29 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+function splitPop(path) {
+  return path.split('/').pop();
+}
+
+function Tab({ href, label }) {
+  const path = usePathname();
+  const isCurrent = splitPop(href) === splitPop(path);
+  const classes = isCurrent
+        ? "inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500"
+        : "inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+
+  return (
+    <a href={href} aria-current={isCurrent ? "page" : null} className={classes}>{label}</a>
+  )
+}
+
 function Tabbar({ tabs }) {
   return (
     <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-      {tabs.map((tab) => (
-        <li key={tab.label} className="me-2">
-          <a href={tab.href} className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">{tab.label}</a>
-          {/*
-             <a href="/forms/data" aria-current="page" className="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Data</a>
-           */}
+      {tabs.map(({ href, label }) => (
+        <li key={label} className="me-2">
+          <Tab href={href} label={label} />
         </li>
       ))}
     </ul>
