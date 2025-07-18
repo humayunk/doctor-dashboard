bb99a72a774a5b52590c49187ba8eefc424d959b
Ryan P. O'Connell <ryan@healthdatasafe.org>
2025-07-18 16:13:57 -0400

    Add formatting and linting tools

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
 import js from "@eslint/js";
 import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import perfectionist from "eslint-plugin-perfectionist";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist/"]),
+  globalIgnores([".next/", "dist/"]),
+  eslintConfigPrettier,
+  perfectionist.configs["recommended-natural"],
   pluginReact.configs.flat.recommended,
   {
     files: ["src/**/*.{cjs,js,jsx,mjs}"],
