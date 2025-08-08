1ba342c0920b25044db26fb772e9b4428f16ad51
Ryan P. O'Connell <ryan@healthdatasafe.org>
2025-08-07 20:39:24 -0400

    Fix hardcoding

--- app/routes/layouts/sidebar.tsx
+++ app/routes/layouts/sidebar.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 import { Sidebar } from "@/components/sidebar";
 
 export default function SidebarLayout() {
-  const user = "drryan";
+  const user = localStorage.getItem("user");
   return (
     <>
       <Sidebar user={user} />
